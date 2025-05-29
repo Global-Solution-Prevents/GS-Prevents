9d56527e2cf74bb7e1e71413310da81afb340d08
Thomas Mineu Fontes <151870487+thomasmfontes@users.noreply.github.com>
2025-05-29 17:18:48 -0300

    Novo visual

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("JS carregado.");
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+
+
+hamburger.addEventListener("click", () => 
+nav.classList.toggle("active"));
+
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector(".header");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      header.classList.add("shrink");
+    } else {
+      header.classList.remove("shrink");
+    }
+  });
+});
